cb0c66e4c57d887f2ebcecb0e3696f65f8233003
Olesia Ivanova <ivanova.ov_87@mail.ru>
2022-07-30 00:36:43 +0300

    перый коммит задания

---
+++ script.js
@@ -0,0 +1,13 @@
+const title = "My first class";
+const screens = "Экран";
+const screenPrice = 25;
+const rollback = "Отмена";
+const fullPrice = 200;
+const adaptive = {
+    name: "Lesya",
+    age: 35
+}
+
+alert('В добрый путь!');
+
+console.log('Поехали!!!');
