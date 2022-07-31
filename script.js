bf9b655ac69c53e340b5d5ac94e03fc9cf348133
Olesia Ivanova <ivanova.ov_87@mail.ru>
2022-07-31 09:17:17 +0300

    первый коммит второго урока

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const title = "My first class";
-const screens = "Экран";
-const screenPrice = 25;
-const rollback = "Отмена";
-const fullPrice = 200;
-const adaptive = {
-    name: "Lesya",
-    age: 35
-}
+let title = "My first class";
+let screens = "Простые, Сложные, Интерактивные";
+let screensPrice = 35;
+let rollback = 50;
+let fullPrice = 120000;
+let adaptive = true
 
-alert('В добрый путь!');
-
-console.log('Поехали!!!');
+//задание 2
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screensPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(", "))
+console.log("Процент отката посреднику за работу: " + (fullPrice * (rollback/100)))
